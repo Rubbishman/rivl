ca64e3d5c49534eabf71a12f5eae289c7021d6bf
Rubbishman <jonathann.bartlett@gmail.com>
2013-10-02 12:50:09 +1300

    Changing to left join on score

--- application/models/competitor_model.php
+++ application/models/competitor_model.php
@@ -13,7 +13,7 @@ class Competitor_model extends CI_Model {
 		$this->db->select('competitor_elo.competitor_id, competitor_elo.elo ,competitor.name, COUNT(CASE WHEN score.rank = 1 THEN 1 ELSE NULL END) wins, COUNT(CASE WHEN score.rank != 1 THEN 1 ELSE NULL END) loses');
 		$this->db->from('competitor');
 		$this->db->join('competitor_elo', 'competitor.id = competitor_elo.competitor_id');
-        $this->db->join('score', 'score.competitor_id = competitor.id');
+        $this->db->join('score', 'score.competitor_id = competitor.id','left');
 		$this->db->where('competitor_elo.competition_id', $competition_id);
         $this->db->group_by('competitor.id');
 		$this->db->order_by('competitor_elo.elo desc, competitor.name asc');
