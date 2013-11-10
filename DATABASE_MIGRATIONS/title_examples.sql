efd53ad43291df48c24ea8395467467930eeeaec
Rubbishman <jonathann.bartlett@gmail.com>
2013-11-10 23:02:05 +1300

    Removed junk from title_examples.sql

--- DATABASE_MIGRATIONS/title_examples.sql
+++ DATABASE_MIGRATIONS/title_examples.sql
@@ -7,25 +7,3 @@ insert into title (competition_id, current_competitor_id,title,description)
 		   (2,6,'Game Hungry','Must. Keep. Playing!'),
 		   (2,16,'Graph Breaker','Gifted with graph breaking powers'),
 		   (2,13,'The Godfather','There''s a lot of money in that white powder.');
-		   
-		   
-'1', 'Dmitri', NULL
-'2', 'Neil', NULL
-'3', 'Nick', NULL
-'4', 'Jonathan', 'jonathann.bartlett@gmail.com'
-'5', 'Liam', NULL
-'6', 'Gerard', NULL
-'7', 'Vlad', NULL
-'8', 'Todd', NULL
-'9', 'Patrick', NULL
-'10', 'Darrin', NULL
-'11', 'Elisabeth', NULL
-'12', 'Ben', NULL
-'13', 'Dean', NULL
-'14', 'Rob', NULL
-'15', 'Dan', NULL
-'16', 'Andrew', NULL
-'17', 'Paul', NULL
-'18', 'Simon', NULL
-'19', 'Kristo', NULL
-'20', 'Darryl', NULL
