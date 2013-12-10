a6dcc12c01bfebf4c8d9c94de7ac64d836458332
Liam Johnston <liamjohnston.nz@gmail.com>
2013-12-10 13:05:20 +1300

    removed scores from history. commented.

--- application/views/templates/competitionMainPage/gameHistoryRow.php
+++ application/views/templates/competitionMainPage/gameHistoryRow.php
@@ -3,12 +3,35 @@
         <% var game2_elo_change = Math.abs(Math.round(game2.elo_change * 10 ) / 10); %>
         <div class="row">
             <!--        <%=game1.game_id%>-->
+
+            <!-- old way: scores -->
+            <!--
             <div class="col-xs-5">
                 <strong><%=game1.name%></strong> vs <%=game2.name%>
             </div>
             <div class="col-xs-3">
                 <strong><%=game1.score%></strong> - <%=game2.score%>
             </div>
+            -->
+
+            <!-- new way when we support clumping and no scores: -->
+            <!--
+            <div class="col-xs-5">
+                <strong><%=game1.name%></strong> vs <%=game2.name%>
+            </div>
+            <div class="col-xs-3">
+                3 games to 1
+            </div>
+            -->
+
+            <!-- may need a scores AND clumping version at some point? -->
+
+            <!-- interim way: no scores or clumping: -->
+            <div class="col-xs-8">
+                <strong><%=game1.name%></strong> vs <%=game2.name%>
+            </div>
+
+
             <div class="col-xs-2">
                 <span class="good"><span class="glyphicon glyphicon-circle-arrow-up"></span> <%= game1_elo_change %></span>
             </div>
--- application/views/templates/playerPage/gameHistoryRow.php
+++ application/views/templates/playerPage/gameHistoryRow.php
@@ -1,11 +1,30 @@
 <script id="competitorGameRowTemplate" type="text/template">
         <div class="row">
+
+            <!-- old way: -->
+            <!--
             <div class="col-m-3  col-xs-6">
                 <a class="playerLink link"><%=vsPlayer%></a>
             </div>
             <div class="col-m-2  col-xs-3">
                 <%=playerScore%>&nbsp;-&nbsp;<%=vsScore%></td>
             </div>
+            -->
+
+            <!-- clumps and no scores: -->
+            <div class="col-m-4  col-xs-5">
+                <a class="playerLink link"><%=vsPlayer%></a>
+            </div>
+            <div class="col-m-6  col-xs-4">
+                 2/3 games won
+            </div>
+
+            <!-- interim solution: -->
+            <!--
+            <div class="col-m-5  col-xs-9">
+                <a class="playerLink link"><%=vsPlayer%></a>
+            </div>
+-->
             <div class="col-m-2  col-xs-3">
                 <% if (playerScore === '11') { %>
                     <span class="good"><span class="glyphicon glyphicon-circle-arrow-up"></span> <%= Math.abs(Math.round(playerElo*10) / 10) %></span>
@@ -13,7 +32,7 @@
                     <span class="bad"><span class="glyphicon glyphicon-circle-arrow-down"></span> <%= Math.abs(Math.round(playerElo*10) / 10) %></span>
                 <% } %>
             </div>
-            <div class="col-m-5 hidden-phone"></div>
+            <!-- what the heck? <div class="col-m-5 hidden-phone"></div>-->
 
         </div>
     </script>
