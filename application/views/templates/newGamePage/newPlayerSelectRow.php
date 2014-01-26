7f0811a2184b4cf203f8251a8f337f7fedba547d
Rubbishman <jonathann.bartlett@gmail.com>
2014-01-26 22:30:00 +1300

    Images work!

--- application/views/templates/newGamePage/newPlayerSelectRow.php
+++ application/views/templates/newGamePage/newPlayerSelectRow.php
@@ -1,5 +1,5 @@
     <script id="newPlayerSelectRowTemplate" type="text/template">
         <li class="list-group-item playerSelection" data-competitor_id="<%=competitor_id%>">
-            <img src="img/avatars/2_<%=competitor_id%>_1?ver=2" class="playerSelectAvatar" /> <%=name%>
+            <img src="img/avatars/2_<%=competitor_id%>_1.png?ver=2" class="playerSelectAvatar" /> <%=name%>
         </li>
     </script>
