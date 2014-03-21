00a1debc1135b845ce2ab092620f55441d3d6ed0
Rubbishman <jonathann.bartlett@gmail.com>
2014-03-21 17:06:47 +1300

    Neil icon, drill draft.

--- DATABASE_MIGRATIONS/ideas/drill_tables.sql
+++ DATABASE_MIGRATIONS/ideas/drill_tables.sql
@@ -1,2 +1,2 @@
-CREATE TABLE drill (id BIGINT, competition_id BIGINT, summary VARCHAR(255), description TEXT, step_size INT);
+CREATE TABLE drill (id BIGINT, competition_id BIGINT, title VARCHAR(100), summary VARCHAR(555), instructions TEXT, external_link VARCHAR(200), step_size INT);
 CREATE TABLE drill_stats (id BIGINT, drill_id BIGINT, competitor_id BIGINT, created TIMESTAMP, score INT);
